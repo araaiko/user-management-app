c4f65e968b579fa0a796fa0950d9bb2f0d8245bd
araaiko <araaiko.122@gmail.com>
2022-11-18 11:41:37 +0900

    Component split of login screen

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,6 +1,9 @@
 /* 外部import */
 import { FC, memo } from "react";
-import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+
+/* 内部import */
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: FC = memo(() => {
   return (
@@ -14,9 +17,7 @@ export const Login: FC = memo(() => {
 
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder={"ユーザーID"} />
-          <Button bg={"teal.400"} color={"white"} _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
