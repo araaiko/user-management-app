a47ead387cd2145f8b1d139279747dd484f927ab
araaiko <araaiko.122@gmail.com>
2022-11-18 15:21:35 +0900

    Login function implemented

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -3,14 +3,24 @@ import { FC, memo, ReactNode } from "react";
 import { Button } from "@chakra-ui/react";
 
 type Props = {
-  children: ReactNode;
+  children:  ReactNode;
+  disabled?: boolean;
+  loading?:  boolean;
+  onClick:   () => void;
 };
 
 export const PrimaryButton: FC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, disabled = false, loading = false, onClick } = props;
 
   return (
-    <Button bg={"teal.400"} color={"white"} _hover={{ opacity: 0.8 }}>
+    <Button
+      bg={"teal.400"}
+      color={"white"}
+      _hover={{ opacity: 0.8 }}
+      disabled={disabled || loading}
+      isLoading={loading}
+      onClick={onClick}
+    >
       {children}
     </Button>
   );
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,11 +1,25 @@
 /* 外部import */
-import { FC, memo } from "react";
+import { ChangeEvent, FC, memo, useState } from "react";
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 
 /* 内部import */
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: FC = memo(() => {
+  const { login, loading } = useAuth();
+  const [userId, setUserId] = useState("");
+
+  // inputの入力値更新
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  }
+
+  // ログイン機能
+  const onClickLogin = () => {
+    login(userId);
+  }
+
   return (
     <Flex align={"center"} justify={"center"} height={"100vh"}>
       <Box bg={"white"} w={"sm"} p={4} borderRadius={"md"} shadow={"md"}>
@@ -16,8 +30,18 @@ export const Login: FC = memo(() => {
         <Divider my={4} />
 
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder={"ユーザーID"} />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input
+            placeholder={"ユーザーID"}
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            disabled={userId === ""}
+            onClick={onClickLogin}
+            loading={loading}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
