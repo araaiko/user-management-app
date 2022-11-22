fd2ca8052503cb5aa071064af50bc6e23d274024
araaiko <araaiko.122@gmail.com>
2022-11-22 12:22:36 +0900

    Implement functionality to hold logged-in user information in context.

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 /* 内部import */
 import theme from "./theme/theme";
 import { Router } from "./router/Router";
-
+import { LoginUserProvider } from "./providers/LoginUserProvider";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Router />
+      <LoginUserProvider>
+        <Router />
+      </LoginUserProvider>
     </ChakraProvider>
   );
 }
